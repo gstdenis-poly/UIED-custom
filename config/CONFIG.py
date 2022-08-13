4f536528836ef6bc229a630f2350b51fb9eb7bab
gstdenis-poly <gabriel.st-denis@polymtl.ca>
2022-08-13 14:26:57 -0400

    Put back default value of COLOR in CONFIG

--- config/CONFIG.py
+++ config/CONFIG.py
@@ -14,22 +14,20 @@ class Config:
         # self.element_class = ['Button', 'CheckBox', 'Chronometer', 'EditText', 'ImageButton', 'ImageView',
         #                       'ProgressBar', 'RadioButton', 'RatingBar', 'SeekBar', 'Spinner', 'Switch',
         #                       'ToggleButton', 'VideoView', 'TextView']
-        self.element_class = ['Button', 'CheckBox', 'EditText', 'RadioButton']
+        self.element_class = ['Button', 'CheckBox', 'EditText', 'RadioButton', 'TextView']
         self.class_number = len(self.element_class)
 
         # setting EAST (ocr) model
         self.EAST_PATH = '/content/drive/MyDrive/Polytechnique/INF6909/server/models/east_icdar2015_resnet_v1_50_rbox'
 
-        # self.COLOR = {'Button': (0, 255, 0), 'CheckBox': (0, 0, 255), 'Chronometer': (255, 166, 166),
-        #               'EditText': (255, 166, 0),
-        #               'ImageButton': (77, 77, 255), 'ImageView': (255, 0, 166), 'ProgressBar': (166, 0, 255),
-        #               'RadioButton': (166, 166, 166),
-        #               'RatingBar': (0, 166, 255), 'SeekBar': (0, 166, 10), 'Spinner': (50, 21, 255),
-        #               'Switch': (80, 166, 66), 'ToggleButton': (0, 66, 80), 'VideoView': (88, 66, 0),
-        #               'TextView': (169, 255, 0), 'NonText': (0,0,255),
-        #               'Compo':(0, 0, 255), 'Text':(169, 255, 0), 'Block':(80, 166, 66)}
-        self.COLOR = {'Button': (0, 255,0), 'CheckBox': (0, 0, 255), 'EditText': (255, 166, 0), 
-                      'RadioButton': (166, 166, 166)}
+        self.COLOR = {'Button': (0, 255, 0), 'CheckBox': (0, 0, 255), 'Chronometer': (255, 166, 166),
+                      'EditText': (255, 166, 0),
+                      'ImageButton': (77, 77, 255), 'ImageView': (255, 0, 166), 'ProgressBar': (166, 0, 255),
+                      'RadioButton': (166, 166, 166),
+                      'RatingBar': (0, 166, 255), 'SeekBar': (0, 166, 10), 'Spinner': (50, 21, 255),
+                      'Switch': (80, 166, 66), 'ToggleButton': (0, 66, 80), 'VideoView': (88, 66, 0),
+                      'TextView': (169, 255, 0), 'NonText': (0,0,255),
+                      'Compo':(0, 0, 255), 'Text':(169, 255, 0), 'Block':(80, 166, 66)}
 
     def build_output_folders(self):
         # setting data flow paths
